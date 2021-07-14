6cc33d6fc7a28c989cc8ae81ded4329c0080b19d
marinacharris <85807129+marinacharris@users.noreply.github.com>
2021-07-13 21:35:34 -0500

    Add files via upload

---
+++ pruebas/app/src/main/java/com/example/pruebas/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.pruebas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private B
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
